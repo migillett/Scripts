0e2d46858742eb4e7e56a13e03d947eb2757ebd2
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-01-12 16:02:16 -0500

    working V1.0

--- kumo.py
+++ kumo.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env /usr/bin/python
 
+# Forked from https://github.com/szumlins/Scripts on Jan 12, 2021
+# Adapted to Python 3
+
 '''
 This script is a simple tool to automate getting and setting of KUMO cross points via python
 On get success, the script returns the source 
@@ -16,84 +19,92 @@ import argparse
 import json
 import sys
 
-parser = argparse.ArgumentParser(description='Set and Get AJA KUMO cross points.  On get success, the script returns the source.  On set success, the script returns the source and destination separated by a comma.  On failure, the script returns -1')
-parser.add_argument('-d','--get_xpt',dest='dest',metavar="CROSSPOINT",type=int,nargs=1,help="Get crosspoint source for destination")
-parser.add_argument('-s','--set_xpt',dest='source',metavar="CROSSPOINT",type=int,nargs=1,help="Set crosspoint source for destination (defined by -d)")
-parser.add_argument('-a','--address',dest='kumo',metavar="ADDRESS",type=str,nargs=1,help="IP address or DNS name of KUMO")
-parser.add_argument('-e','--print_error',dest='err',action='store_true',default=False,help="Print out all error codes and exit")
+parser = argparse.ArgumentParser(
+    description='Set and Get AJA KUMO cross points. On get success, the script returns the source. On set success, the script returns the source and destination separated by a comma. On failure, the script returns -1')
+parser.add_argument('-d', '--get_xpt', dest='dest', metavar="CROSSPOINT", type=int, nargs=1,
+                    help="Get crosspoint source for destination")
+parser.add_argument('-s', '--set_xpt', dest='source', metavar="CROSSPOINT", type=int, nargs=1,
+                    help="Set crosspoint source for destination (defined by -d)")
+parser.add_argument('-a', '--address', dest='kumo', metavar="ADDRESS", type=str, nargs=1,
+                    help="IP address or DNS name of KUMO")
 args = parser.parse_args()
 
 
-if args.err is True:
-	sys.exit('''
-	Error Codes:
-	
-	-1: can't connect to KUMO (check your URL or network)
-	-2: destination out of range (not enough destinations in router)
-	-3: source is out of range (not enough sources in router)
-	''')
+def error(code):
+    if code == 1:
+        sys.exit("ERROR: Can't connect to KUMO (check your URL or network)")
+    elif code == 2:
+        sys.exit('ERROR: destination out of range (not enough destinations in router)')
+    elif code == 3:
+        sys.exit('ERROR: source is out of range (not enough sources in router)')
+
 
 if args.dest is None:
-	sys.exit("No destination defined, exiting.")
+    sys.exit("No destination defined, exiting.")
 
 if args.kumo is None:
-	sys.exit("No router defined, exiting.")
+    sys.exit("No router defined, exiting.")
 
-#set our kumo URL
+# set our Kumo URL
 kumo = 'http://' + args.kumo[0] + '/options'
 
-#get kumo source count (sc)
+# get Kumo source count (sc)
 try:
-	r = requests.get(kumo + '?action=get&paramid=eParamID_NumberOfSources')
+    r = requests.get(kumo + '?action=get&paramid=eParamID_NumberOfSources')
+    j = json.loads(r.text)
+    sc = j['value']
 except requests.ConnectionError:
-	print -1
-	exit()
-j = json.loads(r.text)
-sc = j['value']
+    print(error(1))
 
-#get kumo destination count (dc)
+# get Kumo destination count (dc)
 try:
-	r = requests.get(kumo + '?action=get&paramid=eParamID_NumberOfDestinations')
+    r = requests.get(kumo + '?action=get&paramid=eParamID_NumberOfDestinations')
+    j = json.loads(r.text)
+    dc = j['value']
 except requests.ConnectionError:
-	print -1
-	exit()
-j = json.loads(r.text)
-dc = j['value']
+    error(1)
 
 # if there isn't a source set on the cli, simply look up the source of the supplied destination
 if not args.source:
-	# if destination is larger than router size, exit with error
-	if args.dest[0] > int(dc):
-		print -2
-		exit()	
-	#try getting source for defined destination
-	try:
-		r = requests.get(kumo + '?action=get&paramid=eParamID_XPT_Destination' + str(args.dest[0]) + '_Status',timeout=0.2)
-	except requests.ConnectionError:
-		print -1
-		exit()
-	j = json.loads(r.text)
-	#if we get an error, print it, other wise print out source	
-	if j['value'] == '-1':
-		print -2
-	else:
-		print j['value']
+    # if destination is larger than router size, exit with error
+    if args.dest[0] > int(dc):
+        error(2)
+
+    # try getting source for defined destination
+    try:
+        r = requests.get(
+            kumo + '?action=get&paramid=eParamID_XPT_Destination' + str(args.dest[0]) + '_Status',
+            timeout=0.2)
+    except requests.ConnectionError:
+        error(1)
+
+    j = json.loads(r.text)
+    # if we get an error, print it, other wise print out source
+    if j['value'] == '-1':
+        error(2)
+    else:
+        print(j['value'])
+
 else:
-	#if source is larger than router size, exit with error
-	if args.source[0] > int(sc):
-		print -3
-		exit()
-	# there is a source defined, so switch it
-	post_data = {'paramName': 'eParamID_XPT_Destination' + str(args.dest[0]) + '_Status','newValue': str(args.source[0])}
-	try:	
-		r = requests.post(kumo,data=post_data,timeout=0.2)
-	except requests.ConnectionError:
-		print -1
-		exit()
-		
-	j = json.loads(r.text)
-	#if we have an error, just return -2, if not return source,destination
-	if j['value'] == '-1':
-		print -2
-	else:
-		print j['value'] + ',' + str(args.dest[0])
+    # if source is larger than router size, exit with error
+    if args.source[0] > int(sc):
+        error(3)
+
+    # there is a source defined, so switch it
+    post_data = {
+        'paramName': 'eParamID_XPT_Destination' + str(args.dest[0]) + '_Status',
+        'newValue': str(args.source[0])
+    }
+
+    try:
+        r = requests.post(kumo, data=post_data, timeout=0.2)
+    except requests.ConnectionError:
+        error(1)
+
+    j = json.loads(r.text)
+
+    # if we have an error, just return -2, if not return source,destination
+    if j['value'] == '-1':
+        error(2)
+    else:
+        print(j['value'] + ',' + str(args.dest[0]))
